e109657ebbc983725f58013cb61ff50b7666eb0f
swislockikacper <k.swislocki96@gmail.com>
2019-03-10 14:10:35 +0100

    Added solutions to RabbitMQ

---
+++ Receiver/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Messages:");
+
+            using (var connection = GetConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "message",
+                        arguments: null,
+                        exclusive: false,
+                        durable: false,
+                        autoDelete: false);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, eventArgs) =>
+                    {
+                        var message = eventArgs.Body;
+                        Console.WriteLine(Encoding.UTF8.GetString(message));
+                    };
+
+                    channel.BasicConsume(queue: "message",
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        private static IConnection GetConnection()
+           => new ConnectionFactory()
+           {
+               HostName = "localhost"
+           }
+           .CreateConnection();
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending:");
+
+            using (var connection = GetConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "message",
+                        arguments: null,
+                        exclusive: false,
+                        autoDelete: false,
+                        durable: false);
+
+                    for (int i = 0; i < 15; i++)
+                    {
+                        var message = Encoding.UTF8.GetBytes($"Message {i}");
+
+                        Thread.Sleep(1000);
+
+                        channel.BasicPublish(exchange: "",
+                            routingKey: "message",
+                            basicProperties: null,
+                            body: message);
+
+                        Console.WriteLine(Encoding.Default.GetString(message));
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private static IConnection GetConnection()
+            => new ConnectionFactory()
+            {
+                HostName = "localhost"
+            }
+            .CreateConnection();
+    }
+}
